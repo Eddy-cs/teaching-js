8ec474dbc145c72163ef6c69caac190b631679e3
Eddy-cs <102255380+Eddy-cs@users.noreply.github.com>
2022-08-19 10:00:05 -0400

    Added exercise1 to class2

---
+++ class2/exercise1/script.js
@@ -0,0 +1,33 @@
+////////////////  Parte 1  ////////////////
+
+// 1. Crear un <img> element con javascript y añadilro al html
+//
+// 2. Utilizar el endpoint https://api.imgflip.com/get_memes
+//    del API imgflip para objeter un objeto json
+//
+// 3. Convertir el objeto json en un objeto de javascrip
+//
+// 4. Analizar el objeto que obtenido y lograr mostrar en la consola el url
+//    de la imagen 17
+//
+// 5. Cambiar el valor de la propiedad src de la imagen previamente creada
+//    a el url obtenido con fetch
+
+/////////// Tu codigo aqui
+
+////////////////////////////////
+
+////////////////  Parte 2  ////////////////
+
+// 1. Asignar el data obtenido con fetch a una variable
+//
+// 2. Añadir un boton con un click event listener
+//
+// 3. Crear y aplicar a el event listener, una funcion que
+//    rote la imagen que se presenta en el html
+//
+// 4. Corregir bugs
+
+/////////// Tu codigo aqui
+
+////////////////////////////////
